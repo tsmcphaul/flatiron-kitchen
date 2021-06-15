b1e1811d34dd7b62c351f71542c5b1fa2ad582da
Tammarra McPhaul <shaemc22@gmail.com>
2021-06-14 21:25:09 -0400

    commit tree 28d823e3d9d2d83212e560bf2279899a8406755e

---
+++ app/controllers/recipe_ingredients_controller.rb
@@ -0,0 +1,21 @@
+class RecipeIngredientsController < ApplicationController
+
+    def new
+        
+    end
+   
+    def show
+    end
+   
+    def create
+    end
+   
+    def index
+        
+    end
+   
+    def edit
+    end
+   
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :recipe_ingredients
+  resources :ingredients
+  resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
